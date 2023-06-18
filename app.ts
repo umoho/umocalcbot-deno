cd2341bb3901f15b057ab4a2fd60d0d2a0324a5f
umoho <my.umoho@hotmail.com>
2023-06-18 22:53:41 +0800

    add inline query

--- app.ts
+++ app.ts
@@ -6,22 +6,55 @@ const bot = new Bot(token);
 
 console.log("Bot started.");
 
+const helpText =
+    `You can just send the expression you want to calculate to me `
+    + `or use command <code>/calc ...</code>\n`
+    + `Help:\n`
+    + `  Available operations: <code>+</code> (plus), <code>-</code> (minus), <code>*</code> (multi), <code>/</code> (divide)\n`
+    + `  Use parentheses <code>(</code> and <code>)</code> to increase priority\n`
+    + `  Multiplication and division have higher priority than addition\n`
+    + `  <code>0</code> before decimal separator can be omitted\n`
+    + `Example:\n`
+    + `  <code>.1 + .1 + .1</code>\n`
+    + `  <code>(20 + 1) * 2</code>`;
+
+bot.on("inline_query", async ctx => {
+    const text = ctx.inlineQuery.query;
+    console.log(text);
+    try {
+        const result = calculate(text);
+        await ctx.answerInlineQuery([
+            {
+                type: "article",
+                id: "calculator-answer",
+                title: "Result",
+                input_message_content: {
+                    message_text: `<code>${text}</code> = <code>${result}</code>`,
+                    parse_mode: "HTML",
+                }
+            }
+        ]);
+    } catch (err) {
+        await ctx.answerInlineQuery([
+            {
+                type: "article",
+                id: "calculator-error",
+                title: "Error",
+                input_message_content: {
+                    message_text: `Error while calculating <code>${text}</code>:\n${err}`,
+                    parse_mode: "HTML",
+                }
+            }
+        ]);
+    }
+});
+
 bot.command(["start", "help"], async ctx => {
     const messageId = ctx.message?.message_id;
-    await ctx.reply(
-        `You can just send the expression you want to calculate to me `
-        + `or use command <code>/calc ...</code>\n`
-        + `Help:\n`
-        + `  Available operations: <code>+</code> (plus), <code>-</code> (minus), <code>*</code> (multi), <code>/</code> (divide)\n`
-        + `  Use parentheses <code>(</code> and <code>)</code> to increase priority\n`
-        + `  Multiplication and division have higher priority than addition\n`
-        + `  <code>0</code> before decimal separator can be omitted\n`
-        + `Example:\n`
-        + `  <code>.1 + .1 + .1</code>\n`
-        + `  <code>(20 + 1) * 2</code>`, {
+    await ctx.reply(helpText, {
             reply_to_message_id: messageId,
             parse_mode: "HTML",
-        });
+    });
 });
 
 bot.command("calc", async ctx => {
